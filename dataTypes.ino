e4866563cd52e0f52f8dd625d147698e108446a2
regno001 <rwt7454@gmail.com>
2025-12-02 17:44:26 +0530

    Create dataTypes.ino

---
+++ dataTypes.ino
@@ -0,0 +1,21 @@
+char var = 65;
+char var2 = 'A';
+int a=10;
+unsigned int b= 20;
+fload c =3.02;
+double d  =  4.21;
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ Serial.println(var);
+ Serial.println(var2);
+Serial.println(a);
+Serial.println(b);
+Serial.println(c);
+Serial.println(d);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+} 

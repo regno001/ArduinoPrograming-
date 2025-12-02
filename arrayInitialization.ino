093bcc0282b5be0e5ef4f5d9c8c73a470bdfd34f
regno001 <rwt7454@gmail.com>
2025-12-02 18:04:38 +0530

    Update arrayInitialization.ino

--- arrayInitialization.ino
+++ arrayInitialization.ino
@@ -1,8 +1,13 @@
 int val[]={1,2,3,4,5,6};
+String a={"Rahul","Vipul","Sumit"}
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
  Serial.println(val[2]);
+ for(int i=0;i<3;i++){
+  Serial.println(a);
+ }
+
 }
 
 void loop() {

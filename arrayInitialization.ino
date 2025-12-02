f974c9aa2c65b875674d1a6a334326f66546a94a
regno001 <rwt7454@gmail.com>
2025-12-02 18:00:02 +0530

    Initialize array and print third element

---
+++ arrayInitialization.ino
@@ -0,0 +1,11 @@
+int val[]={1,2,3,4,5,6};
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ Serial.println(val[2]);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
